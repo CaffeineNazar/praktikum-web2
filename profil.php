dbe593c8132568f5a34911532be835e6606e1b0b
CaffeineNazar <140259313+CaffeineNazar@users.noreply.github.com>
2024-06-05 13:30:41 +0800

    Initial commit

---
+++ profil.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/foto.jpg" alt="nazar" width="190" height="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">06 1510 838 / 2210010269</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Nazarul Iman</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Amuntai, 30 April 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">089609682030</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">muhammadnazaruliman30@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/Mhmmd-nazar" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/fb.png" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/ig.jpg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.png" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/Muhammad-Nazarul-Iman" target="_blank">
+                                <img src="img/medsos/linkedin.jpg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
